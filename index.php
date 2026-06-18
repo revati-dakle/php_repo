80e40bd39d6f4fc644f0b535b6ada59307658307
revati-dakle <revatidakle@gmail.com>
2026-06-18 09:46:27 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>PHP Docker App</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Hello from PHP running in Docker!</h1>";
+echo "<p>Current server time: " . date('Y-m-d H:i:s') . "</p>";
+echo "</body>";
+echo "</html>";
+?>
